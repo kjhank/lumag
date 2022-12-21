a3b8e5feb12b234c91886a5cea2118bda6f5359a
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2022-12-21 14:37:54 +0100

    add fallback for decorative images in WPImage component

--- src/components/WPImage/WPImage.tsx
+++ src/components/WPImage/WPImage.tsx
@@ -5,7 +5,7 @@ export const WPImage = ({
   imageData, className, isLazy = true, alt,
 }: WPImageProps) => {
   const { media_details: meta, source_url: pngPath } = imageData;
-  const webpPath = `${imageData.source_url.replaceAll('wp-content/', 'wp-content/uploads-webpc/')}.webp`;
+  const webpPath = `${imageData.source_url.replaceAll('wp-content/', 'wp-content/uploads-webpc/')}.webp`; // this component is set up to generate URLs vbased on the 'Converter for Media plugin', this should be adjusted accordingly if using another plugin
 
   // TODO: avif maybe?
 
@@ -18,7 +18,7 @@ export const WPImage = ({
       <source src={webpPath} type="webp" />
       <source src={pngPath} type="png" />
       <img
-        alt={alt ?? imageData.alt_text}
+        alt={alt ?? imageData.alt_text ?? ''}
         loading={isLazy ? 'lazy' : 'eager'}
         src={pngPath}
       />
