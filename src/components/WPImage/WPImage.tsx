30fe7d818e3ac881f2cfd625b7f7487263b8f252
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2022-12-22 03:04:22 +0100

    additional check for image path

--- src/components/WPImage/WPImage.tsx
+++ src/components/WPImage/WPImage.tsx
@@ -5,7 +5,7 @@ export const WPImage = ({
   imageData, className, isLazy = true, alt = '',
 }: WPImageProps) => {
   const pngPath = imageData?.url;
-  const webpPath = `${pngPath.replaceAll('wp-content/', 'wp-content/uploads-webpc/')}.webp`; // this component is set up to generate URLs vbased on the 'Converter for Media plugin', this should be adjusted accordingly if using another plugin
+  const webpPath = `${pngPath?.replaceAll('wp-content/', 'wp-content/uploads-webpc/')}.webp` ?? null; // this component is set up to generate URLs vbased on the 'Converter for Media plugin', this should be adjusted accordingly if using another plugin
 
   // TODO: avif maybe?
 
